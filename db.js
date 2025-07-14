ab6b91ec4540aa453a738063c1e7c418db3f2e41
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2025-07-14 16:02:10 +0530

    add id to messages

--- db.js
+++ db.js
@@ -1,14 +1,27 @@
+let id = 2;
+
 const messages = [
   {
+    id: 0,
     text: "Hi there!",
     user: "Amando",
     added: new Date().toLocaleString(),
   },
   {
+    id: 1,
     text: "Hello World!",
     user: "Charles",
     added: new Date().toLocaleString(),
   },
 ];
 
-module.exports = messages;
+function addToMessages(user, text, added) {
+  messages.push({ id, user, text, added });
+  id++;
+}
+
+function getMessage(id) {
+  return messages.filter((message) => message.id == id);
+}
+
+module.exports = { messages, addToMessages, getMessage };
