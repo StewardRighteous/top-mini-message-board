ef7338a1c4bc7db45766e96fb178f3dbdd436388
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2025-07-14 14:39:41 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is Running...");
+});
